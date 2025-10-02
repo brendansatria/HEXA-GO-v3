959dfc2d7d2c257af3d2f0bda7bdacfbb9b9e69c
[dyad] <riotbrendan@gmail.com>
2025-10-02 12:21:39 +0700

    [dyad] Replace title with scoreboard - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import { MadeWithDyad } from "@/components/made-with-dyad";
 import HexagonalBoard from "@/components/HexagonalBoard";
 import TileSidebar from "@/components/TileSidebar";
+import Scoreboard from "@/components/Scoreboard";
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
@@ -8,13 +9,8 @@ const Index = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="min-h-screen w-full flex flex-col bg-white dark:bg-gray-950">
-        <div className="text-center py-4 border-b dark:border-gray-800">
-          <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
-            Hexagonal Honeycomb Board
-          </h1>
-          <p className="text-gray-600 dark:text-gray-400 mt-2">
-            Drag tiles from the left and drop them onto the board.
-          </p>
+        <div className="p-4 border-b dark:border-gray-800">
+          <Scoreboard />
         </div>
         <div className="flex flex-1 overflow-hidden">
           <TileSidebar />
