0ac779564c69a6de629708e4e5e8a1dc39887a56
[dyad] <riotbrendan@gmail.com>
2025-10-04 21:35:56 +0700

    [dyad] Added glass bevel effect to hexagons - wrote 1 file(s)

--- src/components/Hexagon.tsx
+++ src/components/Hexagon.tsx
@@ -21,6 +21,8 @@ const Hexagon: React.FC<HexagonProps> = ({ sideLength, className, onClick, word 
     `0,${height / 4}`,
   ].join(' ');
 
+  const filterId = "glassBevelFilter";
+
   return (
     <svg
       width={width}
@@ -30,14 +32,46 @@ const Hexagon: React.FC<HexagonProps> = ({ sideLength, className, onClick, word 
       onClick={onClick}
       className="cursor-pointer"
     >
-      <polygon points={points} className={cn("fill-current stroke-white", className)} strokeWidth="1" />
+      <defs>
+        <filter id={filterId} x="-50%" y="-50%" width="200%" height="200%">
+          {/* Create a height map from the alpha channel */}
+          <feGaussianBlur in="SourceAlpha" stdDeviation="3" result="blur" />
+          
+          {/* Create the lighting effect for a glossy highlight */}
+          <feSpecularLighting in="blur" surfaceScale="5" specularConstant="0.75" specularExponent="20" lightingColor="#ffffff" result="specular">
+            <fePointLight x="-5000" y="-10000" z="20000" />
+          </feSpecularLighting>
+          
+          {/* Composite the lighting on top of the source graphic */}
+          <feComposite in="specular" in2="SourceAlpha" operator="in" result="specular" />
+          <feComposite in="SourceGraphic" in2="specular" operator="arithmetic" k1="0" k2="1" k3="1" k4="0" result="litPaint" />
+
+          {/* Add a subtle drop shadow for depth */}
+          <feGaussianBlur in="SourceAlpha" stdDeviation="2" result="shadowBlur" />
+          <feOffset in="shadowBlur" dx="2" dy="2" result="shadowOffset" />
+          <feFlood floodColor="#000" floodOpacity="0.3" result="shadowColor" />
+          <feComposite in="shadowColor" in2="shadowOffset" operator="in" result="shadow" />
+
+          {/* Merge the shadow and the lit graphic */}
+          <feMerge>
+            <feMergeNode in="shadow" />
+            <feMergeNode in="litPaint" />
+          </feMerge>
+        </filter>
+      </defs>
+      <polygon 
+        points={points} 
+        className={cn("fill-current stroke-white", className)} 
+        strokeWidth="1"
+        filter={`url(#${filterId})`}
+      />
       {word && (
         <text
           x="50%"
           y="50%"
           dominantBaseline="middle"
           textAnchor="middle"
-          className="fill-current text-white font-bold"
+          className="fill-current text-white font-bold drop-shadow-md"
           style={{ fontSize: `${sideLength / 4}px` }}
         >
           {word}
